33dde0b48855556edf13169f1ef492c4d613d031
Shivi-ii <shivivani836409@gmail.com>
2025-03-07 22:47:15 +0530

    added cors policy

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:4173",
+    "https://chatapp-frontend-pwga5e9mm-shivis-projects-6fbccb47.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
